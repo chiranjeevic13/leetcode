f12cad31c9f131c89dde68124d5ff13ead87582c
Chiranjeevi C <131645189+chiranjeevic13@users.noreply.github.com>
2024-05-30 12:47:52 +0530

    Time: 19 ms (27.51%), Space: 55 MB (89.02%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
         int n = nums.length;
         for (int i = 0; i < n - 1; i++) {
                 if (nums[i] == nums[i+1])

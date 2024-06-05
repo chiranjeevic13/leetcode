c19b1eb02d57cc0cff046b81664e10b3da3242d5
Chiranjeevi C <131645189+chiranjeevic13@users.noreply.github.com>
2024-06-05 13:35:43 +0530

    Time: 20 ms (59.94%), Space: 44.9 MB (52.94%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -30,19 +30,5 @@ public class Solution {
 
         return res;
     }
-
-    public boolean isPalindrome(String str) {
-        int left = 0;
-        int right = str.length() - 1;
-
-        while (left < right) {
-            if (str.charAt(left) != str.charAt(right)) {
-                return false;
-            }
-            left++;
-            right--;
-        }
-
-        return true;
-    }
 }
+    

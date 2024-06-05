3162b6ba1af6f26962d9ce0b8d9f4a5504546230
Chiranjeevi C <131645189+chiranjeevic13@users.noreply.github.com>
2024-06-05 13:24:16 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,22 +1,36 @@
 public class Solution {
     public String longestPalindrome(String s) {
-        if (s.length() <= 1) {
-            return s;
-        }
-
-        int maxLen = 1;
-        String maxStr = s.substring(0, 1);
+        int maxLen = 0;
+        String res = "";
 
         for (int i = 0; i < s.length(); i++) {
-            for (int j = i + 1; j <= s.length(); j++) {
-                if (j - i > maxLen && isPalindrome(s.substring(i, j))) {
-                    maxLen = j - i;
-                    maxStr = s.substring(i, j);
+            int l=i;
+            int r=i;
+            while(l>=0 && r<s.length() && s.charAt(l)==s.charAt(r)){
+                if((r-l+1)>maxLen){
+                    res=s.substring(l,r+1);
+                    maxLen=(r-l+1);
                 }
             }
+            l--;
+            r++;
+
+            int le=i;
+            int re=i+1;
+            while(le>=0 && re<s.length() && s.charAt(le)==s.charAt(re)){
+                if((re-le+1)>maxLen){
+                    res=s.substring(le,re+1);
+                    maxLen=(re-le+1);
+                }
+            }
+            le--;
+            re++;
+
+
+
         }
 
-        return maxStr;
+        return res;
     }
 
     public boolean isPalindrome(String str) {

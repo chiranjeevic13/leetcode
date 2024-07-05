127cef32c831e65002c0da5072786cea4465f9a0
Chiranjeevi C <131645189+chiranjeevic13@users.noreply.github.com>
2024-07-05 11:14:00 +0530

    Time: 1 ms (100%), Space: 41.1 MB (90.84%) - LeetHub

---
+++ 2315-count-asterisks/2315-count-asterisks.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int countAsterisks(String s) {
+        int asteriks = 0;
+        int lines = 0;
+        for (char c: s.toCharArray()){
+        switch (c) {
+            case '*': 
+                    if (lines%2==0)
+                        asteriks++; 
+                    break;
+
+            case '|': 
+                    lines++; 
+                    break;
+
+            default: break;
+            }
+        }
+        return asteriks;
+    }
+}

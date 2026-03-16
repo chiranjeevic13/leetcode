daf7f032474d0240325ae855780097912daa7176
Chiranjeevi C <131645189+chiranjeevic13@users.noreply.github.com>
2026-03-16 22:47:20 +0530

    Time: 4 ms (17.03%), Space: 19.4 MB (36.51%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
@@ -1,35 +1,20 @@
-class Solution(object):
-    def reverseWords(self, s):
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        i=len(s)-1
+        words=[]
+
+        while i>=0:
+
+            if s[i]==" ":
+                i-=1
+                continue
+            
+            j=i
+            while (j>=0 and s[j]!=" "):
+                j-=1
+            
+            words.append(s[j+1:i+1])
+
+            i=j-1
         
-        i = 0
-        j = len(s) - 1
-        while i <= j and s[i] == ' ':
-            i += 1
-        while j >= i and s[j] == ' ':
-            j -= 1
-        
-        words = []
-        word = ''
-        while i <= j:
-            if s[i] != ' ':
-                word += s[i]
-            else:
-                if word:
-                    words.append(word)
-                    word = ''
-            i += 1
-        
-        if word:
-            words.append(word)
-        
-        out = []
-        for k in range(len(words) - 1, -1, -1):
-            out.append(words[k])
-        
-        result = ''
-        for k in range(len(out)):
-            if k > 0:
-                result += ' '
-            result += out[k]
-        
-        return result
+        return " ".join(words)
